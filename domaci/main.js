12abfe9beb15fda298fabb45b70e031018b3b8bd
LukaJeremic <62446573+LukaJeremic@users.noreply.github.com>
2020-04-15 20:54:45 +0200

    Add files via upload

---
+++ domaci/main.js
@@ -0,0 +1,59 @@
+function openPage(pageName, elmnt, color) {
+  
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+  
+    document.getElementById(pageName).style.display = "block";
+  
+    elmnt.style.backgroundColor = color;
+  }
+  
+  document.getElementById("defaultOpen").click(); 
+  
+  var acc = document.getElementsByClassName("accordion");
+
+  var i;
+  for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+
+} 
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  var $result = $("#result");
+  var email = $("#email").val();
+  $result.text("");
+
+  if (validateEmail(email)) {
+    $result.text(email + " is valid :)");
+    $result.css("color", "green");
+  } else {
+    $result.text(email + " Make sure it's an email");
+    $result.css("color", "red");
+  }
+  return false;
+}
+
+$("#validate").on("click", validate);
